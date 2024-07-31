cc56738c327921e8013f377764a98ea55e21986f
Micro Fortwonatus <microfortwonatus6@gmail.com>
2024-08-01 06:52:12 +0700

    latihan blade template

---
+++ laravelTable/resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout/master')
+@section('judul')
+    Halaman Home
+@endsection
+@section('content')
+    <h1>Selamat Datang Admin!</h1>
+@endsection
---
+++ laravelTable/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/table', function () {
+    return view('layout.table');
+});
+Route::get('/dataTable', function () {
+    return view('layout.dataTable');
+});
